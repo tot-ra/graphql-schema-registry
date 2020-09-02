49f795ede4cc269e81a3831ee028bc80a201149a
Artjom Kurapov <artkurapov@gmail.com>
2020-09-02 14:02:00 +0300

    changed db models to use less anonymous functions

--- app/database/containers.js
+++ app/database/containers.js
@@ -1,40 +1,44 @@
-const { knex } = require("./index");
+const {knex} = require("./index");
 
-exports.getSchemaContainers = async ({
-	schemaId,
-	limit = 100,
-	offset = 0,
-	trx = knex,
-}) => {
-	return trx("container_schema")
-		.select(
-			"container_schema.id",
-			"container_schema.version",
-			"container_schema.added_time as addedTime",
-			"services.name as serviceName"
-		)
-		.leftJoin("schema", "schema.id", "container_schema.schema_id")
-		.leftJoin("services", "services.id", "schema.service_id")
-		.where("schema_id", "=", schemaId)
-		.orderBy("container_schema.added_time")
-		.offset(offset)
-		.limit(limit);
-};
+const containersModel = {
+	getSchemaContainers: async function ({
+											 schemaId,
+											 limit = 100,
+											 offset = 0,
+											 trx = knex,
+										 }) {
+		return trx("container_schema")
+			.select(
+				"container_schema.id",
+				"container_schema.version",
+				"container_schema.added_time as addedTime",
+				"services.name as serviceName"
+			)
+			.leftJoin("schema", "schema.id", "container_schema.schema_id")
+			.leftJoin("services", "services.id", "schema.service_id")
+			.where("schema_id", "=", schemaId)
+			.orderBy("container_schema.added_time")
+			.offset(offset)
+			.limit(limit);
+	},
 
-exports.getSchemaContainerCount = async ({ schemaId, trx = knex }) => {
-	const result = await trx("container_schema")
-		.count("id", { as: "cnt" })
-		.where("schema_id", "=", schemaId)
-		.andWhere("version", "<>", "latest");
+	getSchemaContainerCount: async function ({schemaId, trx = knex}) {
+		const result = await trx("container_schema")
+			.count("id", {as: "cnt"})
+			.where("schema_id", "=", schemaId)
+			.andWhere("version", "<>", "latest");
 
-	return result[0].cnt;
-};
+		return result[0].cnt;
+	},
 
-exports.isDev = async ({ schemaId, trx = knex }) => {
-	const result = await trx("container_schema")
-		.count("id", { as: "cnt" })
-		.where("schema_id", "=", schemaId)
-		.andWhere("version", "=", "latest");
+	isDev: async function ({schemaId, trx = knex}) {
+		const result = await trx("container_schema")
+			.count("id", {as: "cnt"})
+			.where("schema_id", "=", schemaId)
+			.andWhere("version", "=", "latest");
 
-	return Boolean(result[0].cnt);
-};
+		return Boolean(result[0].cnt);
+	}
+}
+
+module.exports = containersModel;
--- app/database/services.js
+++ app/database/services.js
@@ -1,48 +1,52 @@
-const { knex } = require("./index");
+const {knex} = require("./index");
 
-exports.getActiveServices = async ({ trx = knex } = {}) => {
-	return await trx("services")
-		.select("services.id", "services.name")
-		.where("is_active", true);
-};
+const servicesModel = {
+	getActiveServices: async ({trx = knex} = {}) => {
+		return await trx("services")
+			.select("services.id", "services.name")
+			.where("is_active", true);
+	},
 
-exports.getServicesByIds = async ({ trx = knex, ids } = {}) => {
-	return await trx("services").select("*").whereIn("id", ids);
-};
+	getServicesByIds: async ({trx = knex, ids} = {}) => {
+		return await trx("services").select("*").whereIn("id", ids);
+	},
 
-exports.getServices = async ({ limit = 100, offset = 0, trx = knex } = {}) => {
-	return await trx("services").select("*").limit(limit).offset(offset);
-};
+	getServices: async ({limit = 100, offset = 0, trx = knex} = {}) => {
+		return await trx("services").select("*").limit(limit).offset(offset);
+	},
 
-exports.getService = async ({ trx = knex, name }) => {
-	const service = await trx("services")
-		.select("services.id", "services.name")
-		.where("services.name", name)
-		.andWhere("is_active", true);
+	getService: async ({trx = knex, name}) => {
+		const service = await trx("services")
+			.select("services.id", "services.name")
+			.where("services.name", name)
+			.andWhere("is_active", true);
 
-	return service[0];
-};
+		return service[0];
+	},
 
-exports.insertService = async ({ trx = knex, name }) => {
-	await trx("services").insert({ name });
+	insertService: async ({trx = knex, name}) => {
+		await trx("services").insert({name});
 
-	const service = await exports.getService({ trx, name });
+		const service = await getService({trx, name});
 
-	if (!service) {
-		throw new Error(`Failed to insert service: ${name}`);
+		if (!service) {
+			throw new Error(`Failed to insert service: ${name}`);
+		}
+
+		return service;
+	},
+
+	toggleService: async ({trx = knex, name}, isActive) => {
+		return await trx("services")
+			.update({
+				is_active: isActive,
+			})
+			.where("name", name);
+	},
+
+	deleteService: async ({trx = knex, name}) => {
+		return await trx("services").delete().where("name", name);
 	}
+}
 
-	return service;
-};
-
-exports.toggleService = async ({ trx = knex, name }, isActive) => {
-	return await trx("services")
-		.update({
-			is_active: isActive,
-		})
-		.where("name", name);
-};
-
-exports.deleteService = async ({ trx = knex, name }) => {
-	return await trx("services").delete().where("name", name);
-};
+module.exports = servicesModel;
