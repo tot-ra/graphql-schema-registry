56175dc5af71828e25fdcbb6dfe56ff161cae50c
Artjom Kurapov <artkurapov@gmail.com>
2020-09-09 00:12:27 +0300

    v1.0.3 ### Added - Unit testing with jest & travis integration

---
+++ test/unit/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	rootDir: '../..',
+	resetModules: true,
+	resetMocks: true,
+	coverageReporters: ['json', 'lcov', 'clover'],
+	collectCoverage: true,
+	collectCoverageFrom: ['<rootDir>/app/**/*.js'],
+	testMatch: ['<rootDir>/test/unit/**/*.test.js', '<rootDir>/app/**/*.test.js'],
+	modulePaths: ['<rootDir>']
+};
