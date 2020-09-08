5f35114c8c242b7eccc4d07e6975de2411b6c91b
Artjom Kurapov <artkurapov@gmail.com>
2020-09-09 00:47:18 +0300

    Attempt to add coverage with coveralls.io

--- test/unit/jest.config.js
+++ test/unit/jest.config.js
@@ -2,9 +2,7 @@ module.exports = {
 	rootDir: '../..',
 	resetModules: true,
 	resetMocks: true,
-	coverageReporters: ['json', 'lcov', 'clover'],
-	collectCoverage: true,
-	collectCoverageFrom: ['<rootDir>/app/**/*.js'],
+	collectCoverage: false,
 	testMatch: [
 		'<rootDir>/test/unit/**/*.test.js',
 		'<rootDir>/app/**/*.test.js',
