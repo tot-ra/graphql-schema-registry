118348a372b9f787654229158c6e802ab22b969b
Artjom Kurapov <artkurapov@gmail.com>
2020-09-25 11:00:47 +0300

    fix collation to general

--- migrations/20200908225857_increase_typedefs_limit.js
+++ migrations/20200908225857_increase_typedefs_limit.js
@@ -1,7 +1,7 @@
 exports.up = async function (knex) {
 	await knex.raw(`ALTER TABLE \`schema\`
 		CHANGE \`type_defs\` \`type_defs\` MEDIUMTEXT
-			CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL
+			CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL
 			COMMENT 'Graphql schema definition for specific service';
 	`);
 };
