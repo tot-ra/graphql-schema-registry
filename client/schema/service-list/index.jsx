21c283c9172b73755e6d9f50493a9fdeaa70f9a5
Artjom Kurapov <artkurapov@gmail.com>
2021-10-01 16:59:41 +0300

    update .dockerignore linter

--- client/schema/service-list/index.jsx
+++ client/schema/service-list/index.jsx
@@ -13,11 +13,22 @@ const ServiceList = () => {
 	const { data } = useQuery(SERVICES_LIST);
 
 	if (!data || !data.services || data.services.length === 0) {
-		return <ServiceListColumnEmpty>
-			<p>No federated services found</p>
-			<p>Use <a href={"https://github.com/pipedrive/graphql-schema-registry#post-schemapush"}>POST /schema/push</a> to add one</p>
-
-		</ServiceListColumnEmpty>;
+		return (
+			<ServiceListColumnEmpty>
+				<p>No federated services found</p>
+				<p>
+					Use{' '}
+					<a
+						href={
+							'https://github.com/pipedrive/graphql-schema-registry#post-schemapush'
+						}
+					>
+						POST /schema/push
+					</a>{' '}
+					to add one
+				</p>
+			</ServiceListColumnEmpty>
+		);
 	}
 
 	const match = useRouteMatch('/:serviceName');
