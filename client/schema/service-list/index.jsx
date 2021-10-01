8555a8b55cb364aaa9a77a6ab8b9756c89304bc5
Artjom Kurapov <artkurapov@gmail.com>
2021-10-01 16:06:19 +0300

    minor visual tweaks with navigation

--- client/schema/service-list/index.jsx
+++ client/schema/service-list/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useQuery } from '@apollo/client';
 
-import { ServiceListColumn } from '../styled';
+import { ServiceListColumn, ServiceListColumnEmpty } from '../styled';
 
 import { SERVICES_LIST } from '../../utils/queries';
 import { useRouteMatch, useHistory } from 'react-router-dom';
@@ -13,7 +13,11 @@ const ServiceList = () => {
 	const { data } = useQuery(SERVICES_LIST);
 
 	if (!data || !data.services || data.services.length === 0) {
-		return <div>No registered services</div>;
+		return <ServiceListColumnEmpty>
+			<p>No federated services found</p>
+			<p>Use <a href={"https://github.com/pipedrive/graphql-schema-registry#post-schemapush"}>POST /schema/push</a> to add one</p>
+
+		</ServiceListColumnEmpty>;
 	}
 
 	const match = useRouteMatch('/:serviceName');
