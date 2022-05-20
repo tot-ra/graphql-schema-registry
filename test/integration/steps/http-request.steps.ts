012b66eb93fd7996c881423e47f8b4e9a3a1ba9c
Oscar Serrano <oscar.serrano@manomano.com>
2022-05-20 10:31:40 +0200

    first push test

--- test/integration/steps/http-request.steps.ts
+++ test/integration/steps/http-request.steps.ts
@@ -3,18 +3,20 @@ import fetch from 'node-fetch';
 let response: any;
 import expect from 'expect';
 
-When('I send a "POST" request to {string} with body:', async (url: string, body: string) => {
-	response = await fetch(`http://localhost:3000${url}`, {
-		method: 'POST',
-		body,
-		headers: {'Content-Type': 'application/json'}
-	});
-});
+When(
+	'I send a "POST" request to {string} with body:',
+	async (url: string, body: string) => {
+		response = await fetch(`http://localhost:3000${url}`, {
+			method: 'POST',
+			body,
+			headers: { 'Content-Type': 'application/json' },
+		});
+	}
+);
 
 Then('the response status code should be {int}', async (status: number) => {
 	expect(response?.status).toEqual(status);
 });
-
 
 Then('the response should be in JSON and contain:', async (json) => {
 	const responseBody = await response.json();
