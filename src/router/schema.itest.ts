170933cbff2aff33cdf9f19ce829544750875eb3
Artjom Kurapov <artkurapov@gmail.com>
2022-09-27 17:24:38 +0300

    eslint

--- src/router/schema.itest.ts
+++ src/router/schema.itest.ts
@@ -6,20 +6,19 @@ describe('app/controller/schema', () => {
 		await cleanTables();
 	});
 
-
 	const res = {
-		json: (x) => x
+		json: (x) => x,
 	};
 
 	describe('GET /schema/latest', () => {
 		it('returns empty object if no schemas/services are registered', async () => {
 			const req = {};
 
-			const result = await composeLatest(req, res)
+			const result = await composeLatest(req, res);
 
 			expect(result).toEqual({
 				data: [],
-				success: true
+				success: true,
 			});
 		});
 	});
@@ -28,57 +27,62 @@ describe('app/controller/schema', () => {
 		it('registers schema (with empty url)', async () => {
 			const req = {
 				body: {
-					name: "service_a",
-					version: "v1",
-					type_defs: "type Query { hello: String }",
-					url: "",
-				}
+					name: 'service_a',
+					version: 'v1',
+					type_defs: 'type Query { hello: String }',
+					url: '',
+				},
 			};
 
-			const result = await push(req, res)
+			const result = await push(req, res);
 			expect(result.success).toEqual(true);
 
-			const result2 = await composeLatest({}, res)
+			const result2 = await composeLatest({}, res);
 
 			expect(result2.data[0]).toMatchObject({
-				"is_active": 1,
-				"name": "service_a",
-				"type_defs": "type Query { hello: String }",
-				"url": "",
-				"version": "v1",
+				is_active: 1,
+				name: 'service_a',
+				type_defs: 'type Query { hello: String }',
+				url: '',
+				version: 'v1',
 			});
 		});
 
 		it('registers second version of schema', async () => {
-			let result = await push({
-				body: {
-					name: "service_a",
-					version: "v1",
-					type_defs: "type Query { hello: String }",
-					url: "",
-				}
-			}, res)
+			let result = await push(
+				{
+					body: {
+						name: 'service_a',
+						version: 'v1',
+						type_defs: 'type Query { hello: String }',
+						url: '',
+					},
+				},
+				res
+			);
 			expect(result.success).toEqual(true);
 
-
-			result = await push({
-				body: {
-					name: "service_a",
-					version: "v2",
-					type_defs: "type Query { world: String }",
-					url: "",
-				}
-			}, res)
+			result = await push(
+				{
+					body: {
+						name: 'service_a',
+						version: 'v2',
+						type_defs: 'type Query { world: String }',
+						url: '',
+					},
+				},
+				res
+			);
 			expect(result.success).toEqual(true);
 
-			const result2 = await composeLatest({}, res)
+			const result2 = await composeLatest({}, res);
 
 			expect(result2.data[0]).toMatchObject({
-				"is_active": 1,
-				"name": "service_a",
-				"type_defs": "type Query { world: String }",
-				"url": "",
-				"version": "v2",
+				is_active: 1,
+				name: 'service_a',
+				type_defs: 'type Query { world: String }',
+				url: '',
+				version: 'v2',
 			});
 		});
 
@@ -86,98 +90,115 @@ describe('app/controller/schema', () => {
 			it('requires name', async () => {
 				const req = {
 					body: {
-						name: "",
-						version: "",
-						type_defs: "",
-						url: "",
-					}
+						name: '',
+						version: '',
+						type_defs: '',
+						url: '',
+					},
 				};
 
 				try {
-					const result = await push(req, res)
+					const result = await push(req, res);
 					expect(false).toEqual(true);
 				} catch (e) {
-					expect(e.message).toEqual('"name" is not allowed to be empty');
+					expect(e.message).toEqual(
+						'"name" is not allowed to be empty'
+					);
 				}
 			});
 
 			it('requires version', async () => {
 				const req = {
 					body: {
-						name: "service_a",
-						version: "",
-						type_defs: "",
-						url: "",
-					}
+						name: 'service_a',
+						version: '',
+						type_defs: '',
+						url: '',
+					},
 				};
 
 				try {
-					const result = await push(req, res)
+					const result = await push(req, res);
 					expect(false).toEqual(true);
 				} catch (e) {
-					expect(e.message).toEqual('"version" is not allowed to be empty');
+					expect(e.message).toEqual(
+						'"version" is not allowed to be empty'
+					);
 				}
 			});
 
 			it('requires type_defs', async () => {
 				const req = {
 					body: {
-						name: "service_a",
-						version: "v1",
-						type_defs: "",
-						url: "",
-					}
+						name: 'service_a',
+						version: 'v1',
+						type_defs: '',
+						url: '',
+					},
 				};
 
 				try {
-					const result = await push(req, res)
+					const result = await push(req, res);
 					expect(false).toEqual(true);
 				} catch (e) {
-					expect(e.message).toEqual('"type_defs" is not allowed to be empty');
+					expect(e.message).toEqual(
+						'"type_defs" is not allowed to be empty'
+					);
 				}
 			});
 
 			it('fails if same version is attempted', async () => {
-				let result = await push({
-					body: {
-						name: "service_a",
-						version: "v1",
-						type_defs: "type Query { hello: String }",
-						url: "",
-					}
-				}, res)
+				let result = await push(
+					{
+						body: {
+							name: 'service_a',
+							version: 'v1',
+							type_defs: 'type Query { hello: String }',
+							url: '',
+						},
+					},
+					res
+				);
 				expect(result.success).toEqual(true);
 
 				try {
-					result = await push({
-						body: {
-							name: "service_a",
-							version: "v1", // !!
-							type_defs: "type Query { world: String }",
-							url: "",
-						}
-					}, res)
+					result = await push(
+						{
+							body: {
+								name: 'service_a',
+								version: 'v1', // !!
+								type_defs: 'type Query { world: String }',
+								url: '',
+							},
+						},
+						res
+					);
 					expect(false).toEqual(true);
-				} catch(e){
-					expect(e.message).toEqual("Schema [service_a] and version [v1] already exist in registry. You should not register different type_defs with same version.");
+				} catch (e) {
+					expect(e.message).toEqual(
+						'Schema [service_a] and version [v1] already exist in registry. You should not register different type_defs with same version.'
+					);
 				}
 			});
 
 			it('fails if type B with @extends has no base service', async () => {
 				try {
-					let result = await push({
-						body: {
-							name: 'service_a',
-							version: 'v1',
-							type_defs: `
+					const result = await push(
+						{
+							body: {
+								name: 'service_a',
+								version: 'v1',
+								type_defs: `
 					type Query { hello: String }
 					type A @key(fields: "id") { id: String\n a: String }
 					type B @key(fields: "id") @extends { id: String @external\n a: String }
 					`,
+							},
 						},
-					}, res)
+						res
+					);
 				} catch (e) {
-					expect(e.message).toEqual("Schema validation failed");
+					expect(e.message).toEqual('Schema validation failed');
 				}
 			});
 		});
